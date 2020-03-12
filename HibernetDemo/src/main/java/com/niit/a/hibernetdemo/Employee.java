ce47f3f476504b10887572fbf8e2d48807eec9af
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-03-12 14:15:54 +0530

    First Commit.
    Signed-off-by: Shubham Dubey <shubhamdubey1996@gmail.com>

---
+++ HibernetDemo/src/main/java/com/niit/a/hibernetdemo/Employee.java
@@ -0,0 +1,50 @@
+package com.niit.a.hibernetdemo;
+
+public class Employee {
+	private int id;
+	private int salary;
+	private String firstName;
+	private String lastName;
+
+	public Employee() {
+	}
+
+	public Employee(String fname, String lname, int salary) {
+		this.firstName = fname;
+		this.lastName = lname;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
