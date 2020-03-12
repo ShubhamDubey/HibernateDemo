354c19c554dbeb6cbf3d8131986ece80168db81a
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-03-12 14:54:08 +0530

    Modified App.java

--- HibernetDemo/src/main/java/com/niit/a/hibernetdemo/App.java
+++ HibernetDemo/src/main/java/com/niit/a/hibernetdemo/App.java
@@ -1,13 +1,33 @@
 package com.niit.a.hibernetdemo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+
+		SessionFactory sf = cfg.buildSessionFactory();
+
+		Session ses = sf.openSession();
+
+		Transaction t = ses.beginTransaction();
+
+		Employee e1 = new Employee();
+
+		e1.setFirstName("Shubham");
+		e1.setLastName("Dubey");
+		e1.setSalary(10000);
+
+		ses.save(e1);
+		t.commit();
+		System.out.println("Successfully Saved");
+
+		ses.close();
+		sf.close();
+
+	}
 }
